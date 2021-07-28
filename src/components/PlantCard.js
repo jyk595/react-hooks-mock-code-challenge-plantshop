3e6eb0e01d7a4b65bdde24de32dce16d680a70f2
John <jyk595@gmail.com>
2021-07-28 11:54:42 -0700

    Deliverable 3: Able to make items out of stock

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,23 @@
-import React from "react";
+import { useState } from "react";
 
 function PlantCard({ individualPlant }) {
   const {image = "https://via.placeholder.com/400", name, price, id} = individualPlant;
+  const [inStock, setInStock] = useState(true);
+
+  function stockButtonClick() {
+    setInStock(inStock => !inStock);
+  }
+
 
   return (
     <li className="card" key={id}>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={stockButtonClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={stockButtonClick}>Out of Stock</button>
       )}
     </li>
   );
